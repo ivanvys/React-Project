a022542d55079f2a8eb5f6d0e9a08c033bc94a9c
ivanvys <ivanvysv8@gmail.com>
2022-07-30 21:37:18 +0300

    new

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,12 +1,7 @@
-import { countersSelectors } from "../ReduxCounters/Selectors/selectors";
-import { useSelector } from "react-redux/es/exports";
-
 const Home = () => {
-  const counterOfCounters = useSelector(countersSelectors);
   return (
     <div>
       <h1>Home</h1>
-      <h2>Counters Value:{counterOfCounters.length} </h2>
     </div>
   );
 };
